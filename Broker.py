f6f52eca3b3fa2ec2855b63b5cfac30cf4a7261a
FernandoRuiz87 <164447372+FernandoRuiz87@users.noreply.github.com>
2024-11-17 02:44:52 -0600

    Refactor Broker y Cliente para mejorar la gestión de conexiones y transferencia de video

--- Broker.py
+++ Broker.py
@@ -1,68 +1,103 @@
+import sys
 from helpers.Colores import *
 import helpers.env as env
 from os import system
+from datetime import datetime
+import time
 import threading
 import socket
-import cv2
-import signal
-import sys
-
+from tqdm import tqdm
+import uuid
 class Broker:
     def __init__(self, host, port): # Constructor
         self.host = host
         self.port = port
         self.server = None
         self.nodos = []  # Lista de nodos conectados
-        self.clientes = []  # Lista de clientes conectados
-    
+        self.cliente = None  # Lista de clientes conectados
+        self.identificador = None  # Identificador del video
+        self.address = None  # Dirección del cliente
+        
     def iniciar_servidor(self):
         try:
             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.server.bind((self.host, self.port))
+            self.address = self.server.getsockname()
             self.server.listen()
-            print(f"Servidor iniciado en {PURPLE}{self.host}:{self.port}{GRAY}")
-            print("Esperando conexiones...")
+            print(f"{REVERSE}Servidor iniciado en ({self.host}:{self.port}){RESET}\n")
+            print(f"{CYAN}{BOLD}LOG DEL SERVIDOR{RESET}\n---------------------------------------{RESET}")
         except Exception as e:
-            print(f"Error al iniciar el servidor: {e}")
+            print(f"{RED}[ERROR AL INICIAR EL SERVIDOR] = {YELLOW}{e}{RESET}")
     
     def aceptar_conexiones(self):
         while True:
             conexion, address = self.server.accept()
-            threading.Thread(target=self.procesar_conexion, args=(conexion, address)).start()
+            threading.Thread(target=self.manejador, args=(conexion, address)).start()
     
-    def procesar_conexion(self, conexion, address):
-        try:
-            data = conexion.recv(1024).decode()
-            if data == "[NODO]":
-                self.nodos.append(conexion)
-                print(f"{GREEN}Nodo{GRAY} conectado desde {address}")
-            elif data == "[CLIENTE]":
-                self.clientes.append(conexion)
-                print(f"{CYAN}Cliente{GRAY} conectado desde {address}")
-        except Exception as e:
-            print(f"Error al procesar conexión: {e}")
-    
-    def recibir_video(self,conexion):
-        try:
-            while True:
+    def manejador(self, conexion, address):
+        tipo_conexion = None
+        while True:
+            now = datetime.now()
+            hora_conexion = now.strftime("%Y-%m-%d %H:%M:%S")
+            try:
                 data = conexion.recv(1024)
-                if not data:
-                    print("Terminado")
+                try:
+                    data = data.decode() # Recibir datos del cliente
+                except UnicodeDecodeError:
+                    print("Error al decodificar los datos recibidos")
+                    continue
+                
+                if not data: # Si no hay datos cerrar la conexión
+                    conexion.close()
                     break
-                print("Recibiendo...")
-        except Exception as e:
-            print(f"Error al recibir video: {e}")
+                
+                if data == "[NODO]": # Si el mensaje de identificacion es de un nodo
+                    tipo_conexion = "NODO"
+                    self.nodos.append(conexion)
+                    print(f"{BLUE}[CONEXIÓN_NODO] {YELLOW}{address}{RESET} ~~~ {BLUE}[HORA_CONEXIÓN] {YELLOW}{hora_conexion}{RESET}")
+                    conexion.send("[CONECTADO CON EXITO]".encode()) # Enviar mensaje de confirmación
+                    
+                if data == "[CLIENTE]": # Si el mensaje de identificacion es de un cliente
+                    tipo_conexion = "CLIENTE"
+                    self.cliente = conexion
+                    print(f"{BOLD}{GREEN}[{hora_conexion}] {GREEN}[CONEXIÓN_CLIENTE]{RESET} - {CYAN}[DIRECCION : {address}]{RESET}")
+                    conexion.send("[CONECTADO CON EXITO]".encode())
+                
+                if data == "[VIDEO]": # Si el mensaje de identificacion es de un video
+                    tamaño_video = conexion.recv(1024).decode()
+                    
+                    self.recibir_video(conexion, int(tamaño_video)) 
     
-    
-
+            except Exception as e:
+                hora_error = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f"{BOLD}{RED}[{hora_error}] [DESCONEXION_{tipo_conexion}]{RESET} - {CYAN}[DIRECCION : {address}]{RESET}")
+                conexion.close() 
+                break
+                
+    def recibir_video(self, conexion, tamaño_video): # Recibir video del cliente
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Fecha y hora
+        print(f"{BOLD}{LIGHT_PURPLE}[{timestamp}] [RECIBIENDO_ARCHIVO]{RESET} - {CYAN}[FUENTE : {self.address}] - {RESET}{YELLOW}[TAMAÑO: {tamaño_video} bytes{RESET}]")
+        self.identificador = str(uuid.uuid4()) # Generar identificador único para el video
+        with open(f"Broker_files/Sin_procesar/{self.identificador}.mp4", "wb") as video: 
+            contador = 0
+            
+            while contador <= tamaño_video:
+                datos = conexion.recv(1024)
+                if not datos:
+                    break
+                    
+                if datos == b"[FIN]":
+                    break
+                    
+                video.write(datos)
+                contador += len(datos)        
+        
+            
 if __name__ == "__main__":
     system("cls") # Limpiar consola
-    Broker = Broker(env.BROKER_HOST, env.BROKER_PORT) # Crear instancia de Broker
-    Broker.iniciar_servidor() # Iniciar servidor
+    
+    broker = Broker(env.BROKER_HOST, env.BROKER_PORT) # Crear instancia de Broker
+    broker.iniciar_servidor() # Iniciar servidor
 
-    if Broker.server: # Si el servidor se inicio correctamente aceptar conexiones
-        Broker.aceptar_conexiones()
-    
-    
-    
-
+    if broker.server: # Si el servidor se inicio correctamente aceptar conexiones
+        broker.aceptar_conexiones()
--- Cliente.py
+++ Cliente.py
@@ -1,31 +1,61 @@
 from helpers.Colores import *
 import helpers.env as env
-from os import system
 import socket
+import time
+import cv2
+import os
 
 class Cliente:
     def __init__(self, host, port): # Constructor
         self.host = host
         self.port = port
-        self.client = None
-    
+        self.conexion = None
+
     def conectar_a_broker(self):
         try:
-            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client.connect((self.host, self.port))
-            self.client.send("[CLIENTE]".encode()) # Enviar mensaje para identificar a un cliente en el broker
+            self.conexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.conexion.connect((self.host, self.port))
+            self.conexion.send(b"[CLIENTE]") # Enviar mensaje para identificar a un cliente en el broker
         except Exception as e:
-            print(f"Error al conectar con el broker: {e}")        
+            print(f"Error al conectar con el broker: {e}")
+    
+    def enviar_video(self, ruta_video): # Enviar video al broker
+        try:
+            # Obtener metadata del video
+            tamaño_video = os.path.getsize(ruta_video) # Obtener tamaño del video
+
+            print(tamaño_video)
+            
+            # Enviar metadata del video
+            self.conexion.send(b"[VIDEO]")
+            self.conexion.send(str(tamaño_video).encode())
+
+            # Enviar video al broker
+            with open(ruta_video, "rb") as video:
+                contador = 0
+
+                start_time = time.time()
+
+                while contador <= int(tamaño_video):
+                    datos = video.read(1024)
+                    if not datos:
+                        break
+                    self.conexion.sendall(datos)
+                    contador += len(datos)
+                end_time = time.time()
+            self.conexion.send(b"[FIN]") # Enviar mensaje de finalización
+            print(f"Transferencia completa - Tiempo de envío: {round((end_time - start_time),3)} segundos")
+        except Exception as e:
+            print(f"Error al enviar el video: {e}")
 
 if __name__ == "__main__":
-    system("cls") # Limpiar consola
-    
+    os.system("cls") # Limpiar consola
+  
     Cliente = Cliente(env.BROKER_HOST, env.BROKER_PORT) # Crear instancia de Cliente
     Cliente.conectar_a_broker() # Conectar a broker
     
-    input("Presiona Enter para enviar el video...") # Esperar a que el usuario presione Enter
-    
+    input("Presiona Enter para cerrar la conexión...") # Esperar a que el usuario presione Enter
+
     Cliente.enviar_video("video.mp4") # Enviar video
     
     input("Presiona Enter para cerrar la conexión...") # Esperar a que el usuario presione Enter
-    Cliente.client.close() # Cerrar conexión
